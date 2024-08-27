ed23eabee873072da1e6c1715a57cdb101d1e060
Leywin_Anthony <leywin.anthony@gmail.com>
2024-08-27 03:35:35 +0200

    Portfolio v1 beaucoup de petit changement a venir

---
+++ index.js
@@ -0,0 +1,13 @@
+// let lastscroll = 0;
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY < lastscroll) {
+//         navbar.style.top = "0px";
+//         navbar.style.backdropFilter = "blur(10px)";
+//         logo.style.visibility = "hidden";
+
+
+//     } else {
+//         navbar.style.top = "-160px";
+//     }
+//     lastscroll = window.scrollY;
+// });
