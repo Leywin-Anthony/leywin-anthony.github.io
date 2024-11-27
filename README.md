2d353d1832077a26e0647ad9e63f31e76007fa35
Leywin_Anthony <leywin.anthony@gmail.com>
2024-11-27 17:51:20 +0100

    page produit cree reste a modifier selon le projet et navbar update

--- README.md
+++ README.md
@@ -1,11 +1,39 @@
--- faire la page produit
+<!-- # -- faire la page produit -->
 
---faire l'animation en js de la nav bar
-pour l'animation je pense reprendre le concept de base en cachant la nav
-si tu descend et en la montrant si tu monte limitant l'apect moche du blur
+ensuite adapte le css a tes gouts
+et faire l'event a l'img
 
 --relecture
-
+--responsive
 --pour my experience je pense que line height et
 reequilibrer la police pourrait ressoudre
 le probleme au niveau du responsive
+
+ortus
+la montagne
+le resto
+gamimg campus
+api
+date calculator
+
+\ modifier le texte lorem
+
+open
+corum
+cookimg app
+crypto watch
+
+La Montagne : Exploration des Sommets
+
+Objectif :
+Développer un site web engageant pour célébrer la beauté des montagnes, partager des récits d'aventure, et offrir un espace d'information pour les passionnés de nature et de randonnée.
+
+Technologies Utilisées :
+HTML et CSS Modernes : Pour une présentation visuelle fluide et un design élégant.
+Framework Font Awesome : Pour des icônes modernes et interactives dans le menu et les réseaux sociaux.
+Animations CSS : Ajout d’effets de mouvement subtils pour une expérience immersive.
+
+<!-- Performances du Site :
+Optimisation pour un chargement rapide, garantissant une expérience utilisateur fluide, même avec des connexions faibles.
+Un code organisé et réutilisable, respectant les meilleures pratiques pour une évolutivité future.
+Compatibilité cross-browser et adaptabilité mobile pour maximiser l’accessibilité. -->
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const nav = document.getElementById("nav");
-const navLinks = document.querySelectorAll(".nav_link"); // Sélectionner tous les liens avec la classe 'nav_link'
+const navLinks = document.querySelectorAll(".nav_link"); //
 
 window.addEventListener("scroll", () => {
   let scrollValue =
     (window.scrollY + window.innerHeight) / document.body.offsetHeight;
 
-  // Afficher ou masquer la nav
   if (scrollValue > 0.34) {
     nav.style.visibility = "visible";
     nav.style.opacity = "1";
@@ -15,12 +14,43 @@ window.addEventListener("scroll", () => {
   }
 
   // Supprimer la classe activeLink de tous les liens
+  //   navLinks.forEach((link) => {
+  //     link.classList.remove("activeLink");
+  //   });
+
+  //   navLinks.forEach((link) => {
+  //     if (
+  //       link.getAttribute("href") === "#home" &&
+  //       scrollValue > 0 &&
+  //       scrollValue <= 0.34
+  //     ) {
+  //       link.classList.add("activeLink");
+  //     } else if (
+  //       link.getAttribute("href") === "#about" &&
+  //       scrollValue > 0.34 &&
+  //       scrollValue <= 0.6
+  //     ) {
+  //       link.classList.add("activeLink");
+  //     } else if (
+  //       link.getAttribute("href") === "#book" &&
+  //       scrollValue > 0.6 &&
+  //       scrollValue <= 0.75
+  //     ) {
+  //       link.classList.add("activeLink");
+  //     } else if (
+  //       link.getAttribute("href") === "#portfolio" &&
+  //       scrollValue > 0.75 &&
+  //       scrollValue <= 0.95
+  //     ) {
+  //       link.classList.add("activeLink");
+  //     } else if (link.getAttribute("href") === "#contact" && scrollValue > 0.95) {
+  //       link.classList.add("activeLink");
+  //     }
+  //   });
+  // });
   navLinks.forEach((link) => {
     link.classList.remove("activeLink");
-  });
 
-  // Ajouter la classe activeLink seulement aux liens qui remplissent les conditions
-  navLinks.forEach((link) => {
     if (
       link.getAttribute("href") === "#home" &&
       scrollValue > 0 &&
@@ -48,5 +78,36 @@ window.addEventListener("scroll", () => {
     } else if (link.getAttribute("href") === "#contact" && scrollValue > 0.95) {
       link.classList.add("activeLink");
     }
+  });
+});
+
+/*=============== SERVICES MODAL ===============*/
+// Get the modal
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll("#services__button"),
+  modalClose = document.querySelectorAll(".services__modal-close");
+modalImgs = document.querySelectorAll(".services_img");
+
+// When the user clicks on the button, open the modal
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
+
+modalBtns.forEach((mb, i) => {
+  mb.addEventListener("click", () => {
+    modal(i);
+  });
+});
+modalImgs.forEach((mc, i) => {
+  mc.addEventListener("click", () => {
+    modal(i);
+  });
+});
+
+modalClose.forEach((mc) => {
+  mc.addEventListener("click", () => {
+    modalViews.forEach((mv) => {
+      mv.classList.remove("active-modal");
+    });
   });
 });
